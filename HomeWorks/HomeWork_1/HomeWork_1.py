e40a9a75cc97be4e7ea465c5c92c2b7c4bf01366
Andry Yahoda <andry160621@gmail.com>
2024-09-28 20:36:51 +0300

    1-st commit

--- HomeWorks/HomeWork_1/HomeWork_1.py
+++ HomeWorks/HomeWork_1/HomeWork_1.py
@@ -1,22 +1,13 @@
-def no_space(text):
-    # first option
-    new_text = text.replace(" ", "")
+def replace(new_text):
+    new_text = new_text.replace(" ", "").lower()
     return new_text
-
-    # second option
-    # text = text.split()
-    # text = ''.join(text)
-    # return text
 
 
 input_text = input("Введіть рядок: ")
-text_without_spaces = no_space(input_text)
+text_without_spaces = replace(input_text)
 print("Це текст без пробілів:", text_without_spaces)
 
-reversed_text = ''.join(reversed(text_without_spaces))
-print("Це перевернутий текст:", reversed_text)
-
-if text_without_spaces == reversed_text:
+if text_without_spaces == text_without_spaces[::-1]:
     print("Це паліндром")
 else:
     print("Це не паліндром")
