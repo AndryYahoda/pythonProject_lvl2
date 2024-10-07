ce1b4d1d4c6e260e46e672a96d112b32bfa93759
Andry Yahoda <andry160621@gmail.com>
2024-10-07 13:55:04 +0300

    4th commit

--- HomeWorks/HomeWork4/HomeWork4.py
+++ HomeWorks/HomeWork4/HomeWork4.py
@@ -1,22 +1,9 @@
-# import random
-
-# input_list = [random.choice([0, random.randint(0, 5)]) for _ in range(5)]
-
-
-def move_zeros_to_end(lst):
-    non_zeros = [x for x in lst if x != 0]
-
-    zero_count = lst.count(0)
-
-    return non_zeros + [0] * zero_count
+def move_zeros(lst):
+    non_zeros = [x for x in lst if x != 0 or isinstance(x, bool)]
+    zeros = [x for x in lst if x == 0 and not isinstance(x, bool)]
+    return non_zeros + zeros
 
 
-# Для рандому
-# print("Список:", input_list)
-# result = move_zeros_to_end(input_list)
-# print("Список після переміщення нулів:", result)
-
-# Ваш приклад
-# input_list = ["a", 0, 0, "b", None, "c", "d", 0, 1, False, 0, 1, 0, 3, [], 0, 1, 9, 0, 0, {}, 0, 0, 9]
-# result = move_zeros_to_end(input_list)
-# print("Список після переміщення нулів:", result)
+list = ["a", 0, 0, "b", None, "c", "d", 0, 1, False, 0, 1, 0, 3, [], 0, 1, 9, 0, 0, {}, 0, 0, 9]
+result = move_zeros(list)
+print(result)
