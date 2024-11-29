325dddc5d5b189a992e1d7643b37be59d977cd16
Andry Yahoda <andry160621@gmail.com>
2024-11-29 22:21:55 +0200

    22nd commit

--- HomeWorks/HomeWork_18/HomeWork_18.py
+++ HomeWorks/HomeWork_18/HomeWork_18.py
@@ -7,18 +7,21 @@ with open("part_data_1.txt", "r", encoding="utf-8") as file:
 
 columns = lines[0].strip().split("\t")
 data = [line.strip().split("\t") for line in lines[1:]]
+
 conn = sqlite3.connect(db_name)
 cursor = conn.cursor()
 
 try:
     cursor.execute(f"""
         CREATE TABLE table_no_constraints (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             {", ".join([f"{col} TEXT" for col in columns])}
         );
     """)
 
     cursor.execute(f"""
         CREATE TABLE table_unique_col2 (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             {columns[0]} TEXT,
             {columns[1]} TEXT UNIQUE,
             {", ".join([f"{col} TEXT" for col in columns[2:]])}
@@ -27,6 +30,7 @@ try:
 
     cursor.execute(f"""
         CREATE TABLE table_not_null_col3 (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             {", ".join([f"{columns[i]} TEXT{' NOT NULL' if i == 2 else ''}" for i in range(len(columns))])}
         );
     """)
